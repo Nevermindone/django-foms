00762bc3d5496cb86adaf9387656e7fce1d93b7b
nevermindone <alexandr.jri.bystrov@yandex.ru>
2024-02-08 04:20:25 +0300

    add fields to model

--- apps/FOMS/models.py
+++ apps/FOMS/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class File(models.Model):
-    name = models.CharField()
+    name = models.CharField(blank=True, null=True)
     file = models.FileField(upload_to='uploads/')
 
     def __str__(self):
