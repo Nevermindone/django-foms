4d9f020e0506b18b2d2e1e78147a787b022de0b4
nevermindone <alexandr.jri.bystrov@yandex.ru>
2024-02-09 21:32:24 +0300

    plans

---
+++ apps/FOMS/services/archives_processor.py
@@ -0,0 +1,41 @@
+class Unarchive:
+    def __init__(self):
+        """
+        need pyunpack
+        call db
+        get zip/rar file
+        unarchive
+        save to temp folder
+        """
+
+
+class PdfConverter:
+    def __init__(self):
+        """
+        go to temp folder
+        all files from temp folder to pdf and pdf_temp_folder
+        """
+
+
+class PdfProcessor:
+    def __init__(self, keyword):
+        """
+
+        use everything from jupiter to parse PDFs to dataframe and find keyword
+
+        """
+
+
+class ExcelCreator:
+    def __init__(self):
+        """
+        get instances from pdf processor
+        create an excel
+        """
+
+
+class NotifyEmail:
+    def __init__(self):
+        """
+        add email notification somehow
+        """
